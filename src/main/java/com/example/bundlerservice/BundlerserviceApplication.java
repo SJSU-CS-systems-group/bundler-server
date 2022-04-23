2487840998f5deb8dc851fdc9732cecaaddb194d
Shashank Hegde <shashankvhegde@gmail.com>
2022-04-23 11:32:55 -0700

    Basic Server

---
+++ src/main/java/com/example/bundlerservice/BundlerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bundlerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BundlerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BundlerserviceApplication.class, args);
+	}
+
+}
